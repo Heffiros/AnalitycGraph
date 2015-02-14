4bb298902d3ddc0d2208f96be1af7cb8c2c589e0
alevy <alevy@localhost.localdomain>
2015-02-14 11:37:59 +0100

    Ajout vu rendu script

--- project/app/controllers/home_controller.rb
+++ project/app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
 	def index
 		render :index
 	end
+
+
+	def returnscript
+		#:-> le nom du tableau que max enverra
+		@result = params[:];
+	end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-
+  match '/script', to: 'home#returnscript', via: [:get, :post]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
